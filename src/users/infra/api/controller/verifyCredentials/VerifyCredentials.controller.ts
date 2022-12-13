8eea5a7822e33ef61e99bd35c8fecac0a67ff453
Gabriel Silvestre <gabriel.h.silvestre11@gmail.com>
2022-12-13 15:01:33 -0300

    feat(users/infra/api): add Rmq communication into VerifyCredentialsController

--- src/users/infra/api/controller/verifyCredentials/VerifyCredentials.controller.ts
+++ src/users/infra/api/controller/verifyCredentials/VerifyCredentials.controller.ts
@@ -1,5 +1,11 @@
 import { Controller, UseFilters } from '@nestjs/common';
-import { GrpcMethod } from '@nestjs/microservices';
+import {
+  Ctx,
+  GrpcMethod,
+  MessagePattern,
+  Payload,
+  RmqContext,
+} from '@nestjs/microservices';
 
 import type {
   InputVerifyCredentialsDto,
@@ -8,12 +14,15 @@ import type {
 
 import { VerifyCredentialsUseCase } from '@users/useCase/verifyCredentials/VerifyCredentials.useCase';
 
+import { RmqService } from '@shared/modules/rmq/rmq.service';
+
 import { ExceptionFilterRpc } from '@shared/infra/filter/ExceptionFilter.grpc';
 
 @Controller()
 export class VerifyCredentialsController {
   constructor(
     private readonly verifyCredentialsUseCase: VerifyCredentialsUseCase,
+    private readonly rmqService: RmqService,
   ) {}
 
   @UseFilters(new ExceptionFilterRpc())
@@ -22,5 +31,24 @@ export class VerifyCredentialsController {
     data: InputVerifyCredentialsDto,
   ): Promise<OutputVerifyCredentialsDto | never> {
     return this.verifyCredentialsUseCase.execute(data);
+  }
+
+  @UseFilters(new ExceptionFilterRpc())
+  @MessagePattern('verify_user_credentials')
+  async handleRmq(
+    @Payload() data: InputVerifyCredentialsDto,
+    @Ctx() ctx: RmqContext,
+  ): Promise<OutputVerifyCredentialsDto | never> {
+    try {
+      const result = await this.verifyCredentialsUseCase.execute(data);
+
+      this.rmqService.ack(ctx);
+
+      return result;
+    } catch (err) {
+      this.rmqService.nack(ctx);
+
+      throw err;
+    }
   }
 }

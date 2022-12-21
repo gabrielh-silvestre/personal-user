f236dec2cccaeade1d49e842c9119741d4afd9f4
Gabriel Silvestre <gabriel.h.silvestre11@gmail.com>
2022-12-21 15:54:40 -0300

    feat(users/infra/presenter): crate RestPresenter contract
    - implement CreateUser REST presenter

---
+++ src/users/infra/presenter/rest/create/CreateUser.presenter.ts
@@ -0,0 +1,32 @@
+import type {
+  EmbeddedLink,
+  InputRestPresenterDto,
+  IRestPresenter,
+  OutputRestPresenterDto,
+} from '../Rest.presenter.interface';
+import type { OutputCreateUserDto } from '@users/useCase/create/CreateUser.dto';
+
+export class CreateUserPresenter
+  implements IRestPresenter<OutputCreateUserDto>
+{
+  private static readonly relatedLinks: EmbeddedLink[] = [
+    {
+      self: { _links: { href: '/users/me' } },
+    },
+    {
+      recoverPassword: { _links: { href: '/users/recover-password/{email}' } },
+    },
+  ];
+
+  present(
+    dto: InputRestPresenterDto<OutputCreateUserDto>,
+  ): OutputRestPresenterDto<OutputCreateUserDto> {
+    const { selfLink, data } = dto;
+
+    return {
+      _links: { self: { href: selfLink } },
+      _embedded: CreateUserPresenter.relatedLinks,
+      data,
+    };
+  }
+}

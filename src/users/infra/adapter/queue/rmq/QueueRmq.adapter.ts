d8a9c9b6c0e1c5c1375b09bc1a4b66f7a5cab7c3
Gabriel Silvestre <gabriel.h.silvestre11@gmail.com>
2023-03-13 11:41:17 -0300

    feat(docker-compose): add rabbitmq service
    - add timeout into queue adapter

--- src/users/infra/adapter/queue/rmq/QueueRmq.adapter.ts
+++ src/users/infra/adapter/queue/rmq/QueueRmq.adapter.ts
@@ -1,6 +1,6 @@
 import { Inject, Injectable } from '@nestjs/common';
 import { ClientProxy } from '@nestjs/microservices';
-import { Observable } from 'rxjs';
+import { Observable, timeout } from 'rxjs';
 
 import type { IQueueAdapter } from '../Queue.adapter.interface';
 
@@ -11,7 +11,7 @@ export class QueueRmqAdapter implements IQueueAdapter {
   constructor(@Inject(USER_QUEUE) private readonly client: ClientProxy) {}
 
   send<T>(event: string, data: any): Observable<T> {
-    return this.client.send(event, data);
+    return this.client.send(event, data).pipe(timeout(6000));
   }
 
   emit(event: string, data: any): void {
